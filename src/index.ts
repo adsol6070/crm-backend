5b9b81e6ad2cb6078a4a3f950e5c035222caf9ec
unknown <adsolforweb@gmail.com>
2024-04-08 15:12:33 +0530

    Setup connection with mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import logger from "./config/logger";
+import config from "./config/config";
 
-app.listen(app.get("port"), () => {
-  logger.info(`Listening at http://localhost:${app.get("port")}`);
-});
+let server: any;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(config.mongoose.url);
+    logger.info("Connected to MongoDB");
+
+    server = app.listen(config.port, () => {
+      logger.info(`Server is listening at http://localhost:${config.port}`);
+    });
+  } catch (error) {
+    logger.error(`Error connecting to MongoDB: ${error}`);
+    process.exit(1);
+  }
+};
+
+startServer();
