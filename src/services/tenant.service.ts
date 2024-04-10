05d8588faf6aaa2ccfdbe33a1ae0aa9b202eb27a
jatin goyal <adsolforweb@gmail.com>
2024-04-10 14:29:37 +0530

    Some file changes

---
+++ src/services/tenant.service.ts
@@ -0,0 +1,57 @@
+import Queue from 'bull';
+import { db } from '../config/databse';
+import { bootstrap, getTenantConnection } from './connection.service';
+
+interface Params {
+  tenantName: string;
+  password: string;
+  uuid: string;
+}
+
+const up = async (params: Params): Promise<void> => {
+  const job = new Queue(
+    `setting-up-database-${new Date().getTime()}`,
+    `redis://127.0.0.1:6379`
+  );
+  job.add({ ...params });
+  job.process(async (job, done) => {
+    try {
+      await db.raw(`CREATE ROLE ${params.tenantName} WITH LOGIN PASSWORD '${params.password}';`);
+      await db.raw(`GRANT ${params.tenantName} TO admin;`);
+      await db.raw(`CREATE DATABASE ${params.tenantName} WITH OWNER ${params.tenantName};`);
+      await db.raw(`GRANT ALL PRIVILEGES ON DATABASE ${params.tenantName} TO ${params.tenantName};`);
+
+      await bootstrap();
+      const tenant = getTenantConnection(params.uuid);
+      done();
+    } catch (e) {
+      console.error(e);
+    }
+  });
+};
+
+const down = async (params: Params): Promise<void> => {
+  const job = new Queue(
+    `deleting-database-${new Date().getTime()}`,
+    `redis://127.0.0.1:6379`
+  );
+
+  job.add({ ...params });
+  job.process(async (job, done) => {
+    try {
+      await db.raw(
+        `SELECT pg_terminate_backend(pid) FROM pg_stat_activity
+        WHERE pid <> pg_backend_pid()
+        AND datname = '${params.tenantName}';`
+      );
+      await db.raw(`DROP DATABASE IF EXISTS ${params.tenantName};`);
+      await db.raw(`DROP ROLE IF EXISTS ${params.tenantName};`);
+      await bootstrap();
+      done();
+    } catch (e) {
+      console.error(e);
+    }
+  });
+};
+
+export { up, down };
