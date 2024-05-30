836457dd2c4312dee01c7d314f5a67d06cca9370
unknown <adsolforweb@gmail.com>
2024-05-30 17:29:20 +0530

    Minor changes

--- src/migrations/tenant/20240529120513_create_group_users_table.ts
+++ src/migrations/tenant/20240529120513_create_group_users_table.ts
@@ -1,10 +1,32 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  try {
+    await knex.schema.createTable(
+      "group_users",
+      function (table: Knex.CreateTableBuilder) {
+        table
+          .uuid("group_id")
+          .references("id")
+          .inTable("groups")
+          .onDelete("CASCADE");
+        table
+          .uuid("user_id")
+          .references("id")
+          .inTable("users")
+          .onDelete("CASCADE");
+        table.primary(["group_id", "user_id"]);
+      },
+    );
+  } catch (error) {
+    throw error;
+  }
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  try {
+    await knex.schema.dropTable("group_users");
+  } catch (error) {
+    throw error;
+  }
 }
-
