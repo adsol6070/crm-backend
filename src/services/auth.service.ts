49366d201cacf4766af420f00153a50d76bb9934
unknown <adsolforweb@gmail.com>
2024-06-19 16:24:21 +0530

    Make the lead api permissions based

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -22,7 +22,6 @@ const loginWithEmailAndPassword = async (
 };
 
 const logout = async (connection: Knex, refreshToken: string) => {
-  console.log("logout token 1", refreshToken)
   const refreshTokenDoc = await connection("tokens")
     .select("*")
     .where({
@@ -31,13 +30,13 @@ const logout = async (connection: Knex, refreshToken: string) => {
       blacklisted: false,
     })
     .first();
-    console.log("logout token 2", refreshTokenDoc)
   if (!refreshTokenDoc) {
     return;
   }
 
-  const reponse = await connection("tokens").where({ id: refreshTokenDoc.id }).del();
-  console.log("logout token 3", reponse)
+  const reponse = await connection("tokens")
+    .where({ id: refreshTokenDoc.id })
+    .del();
 };
 
 const refreshAuth = async (connection: Knex, refreshToken: string) => {
