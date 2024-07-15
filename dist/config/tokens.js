97f69a0483883cc63780229533a3340af5c7bc08
unknown <adsolforweb@gmail.com>
2024-07-15 11:47:32 +0530

    One minor change

--- dist/config/tokens.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenTypes = void 0;
-const tokenTypes = {
-    ACCESS: "access",
-    REFRESH: "refresh",
-    RESET_PASSWORD: "resetPassword",
-};
-exports.tokenTypes = tokenTypes;
